1c450e8a3102a6b28ffaaeb3c3e3b6d6a10977bc
Pritha Srivastava <prsrivas@redhat.com>
2020-03-26 18:23:58 +0530

    rgw: Added code for offline OpenId Connect/ OAuth 2.0 tokens if the token is a JWT.
    Resolves: rhbz#1274084
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>
    (cherry picked from commit 6a488a8ffc209c86b49277086124fbeca979b92d)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_web_idp.h
+++ src/rgw/rgw_web_idp.h
@@ -13,17 +13,19 @@ namespace web_idp {
 
 //WebToken contains some claims from the decoded token which are of interest to us.
 struct WebTokenClaims {
-    //Subject of the token
-    string sub;
-    //Intended audience for this token
-    string aud;
-    //Issuer of this token
-    string iss;
-    //Human-readable id for the resource owner
-    string user_name;
+  //Subject of the token
+  std::string sub;
+  //Intended audience for this token
+  std::string aud;
+  //Issuer of this token
+  std::string iss;
+  //Human-readable id for the resource owner
+  std::string user_name;
+  //Client Id
+  std::string client_id;
 };
 
-}; /* namespace web_idp */
-}; /* namespace rgw */
+} /* namespace web_idp */
+} /* namespace rgw */
 
 #endif /* CEPH_RGW_WEB_IDP_H */

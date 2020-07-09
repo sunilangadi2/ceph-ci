4d4a6868cd5d35ce91ce0516b0b5f8107130432f
Ilya Dryomov <idryomov@gmail.com>
2020-07-09 22:27:41 +0200

    test/librbd/fsx: respect rbd_default_map_options in krbd_open()
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit b29dfc4624f3b8286d345ec62894fa70c5f6cf9b)
    (cherry picked from commit dca1d19eaa1d4fa75c3c75054a886d0d9a990e16)
    
    Resolves: rhbz#1927869

--- src/test/librbd/fsx.cc
+++ src/test/librbd/fsx.cc
@@ -960,6 +960,7 @@ const struct rbd_operations librbd_operations = {
 int
 krbd_open(const char *name, struct rbd_ctx *ctx)
 {
+	char buf[1024];
 	char *devnode;
 	int fd;
 	int ret;
@@ -968,7 +969,14 @@ krbd_open(const char *name, struct rbd_ctx *ctx)
 	if (ret < 0)
 		return ret;
 
-	ret = krbd_map(krbd, pool, "", name, "", "", &devnode);
+	ret = rados_conf_get(cluster, "rbd_default_map_options", buf,
+			     sizeof(buf));
+	if (ret < 0) {
+		simple_err("Could not get rbd_default_map_options value", ret);
+		return ret;
+	}
+
+	ret = krbd_map(krbd, pool, "", name, "", buf, &devnode);
 	if (ret < 0) {
 		prt("krbd_map(%s) failed\n", name);
 		return ret;

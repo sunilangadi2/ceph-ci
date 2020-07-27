13955b312b4ac52a04b2e121bf80da7ffbc032cd
Jason Dillaman <dillaman@redhat.com>
2020-07-27 14:36:49 -0400

    librbd: hold ImageCtx::image_lock while computing config overrides
    The 'ImageCtx::apply_metadata' is guaranteed to be thread-safe but
    a future commit will attempt to utilize the cached config override
    set.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit c7104d18e190f3718ec1625376f8c63676096efa)
    (cherry picked from commit 652952f2d74df907637f03eff542a56e02a1ec8c)
    
    Resolves: rhbz#1860057

--- src/librbd/ImageCtx.cc
+++ src/librbd/ImageCtx.cc
@@ -732,6 +732,8 @@ public:
                                 bool thread_safe) {
     ldout(cct, 20) << __func__ << dendl;
 
+    std::unique_lock image_locker(image_lock);
+
     // reset settings back to global defaults
     for (auto& key : config_overrides) {
       std::string value;
@@ -769,6 +771,8 @@ public:
         }
       }
     }
+
+    image_locker.unlock();
 
 #define ASSIGN_OPTION(param, type)              \
     param = config.get_val<type>("rbd_"#param)

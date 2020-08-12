pritha-srivastava/ceph

rgw: adds code for role session to be used an ARN.
Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

src/rgw/rgw_rest_sts.h
@@ -77,9 +77,10 @@ class DefaultStrategy : public rgw::auth::Strategy,
 
   aplptr_t create_apl_web_identity( CephContext* cct,
                                     const req_state* s,
+                                    const string& role_session,
                                     const rgw::web_idp::WebTokenClaims& token) const override {
     auto apl = rgw::auth::add_sysreq(cct, ctl, s,
-      rgw::auth::WebIdentityApplier(cct, ctl, token));
+      rgw::auth::WebIdentityApplier(cct, ctl, role_session, token));
     return aplptr_t(new decltype(apl)(std::move(apl)));
   }
 

sunilangadi2/ceph-ci

Resolves: rhbz#1274084
(cherry picked from commit bebe54b54872e21e07f00d491d8e487737162557)
Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_rest_sts.h
@@ -69,9 +69,10 @@ class DefaultStrategy : public rgw::auth::Strategy,
 
   aplptr_t create_apl_web_identity( CephContext* cct,
                                     const req_state* s,
+                                    const string& role_session,
                                     const rgw::web_idp::WebTokenClaims& token) const override {
     auto apl = rgw::auth::add_sysreq(cct, store, s,
-      rgw::auth::WebIdentityApplier(cct, store, token));
+      rgw::auth::WebIdentityApplier(cct, store, role_session, token));
     return aplptr_t(new decltype(apl)(std::move(apl)));
   }
 
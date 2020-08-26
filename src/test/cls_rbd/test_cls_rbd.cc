trociny/ceph

cls/rbd: method to set mirror snapshot copy progress
Signed-off-by: Mykola Golub <mgolub@suse.com>

src/test/cls_rbd/test_cls_rbd.cc
@@ -2283,6 +2283,16 @@ TEST_F(TestClsRbd, mirror_snapshot) {
   ASSERT_FALSE(nsn->copied);
   ASSERT_EQ(nsn->copy_progress, 0);
 
+  ASSERT_EQ(0, mirror_image_snapshot_set_copy_progress(&ioctx, oid, 2, true,
+                                                       10));
+  ASSERT_EQ(0, snapshot_get(&ioctx, oid, 2, &snap));
+  nsn = boost::get<cls::rbd::MirrorNonPrimarySnapshotNamespace>(
+    &snap.snapshot_namespace);
+  ASSERT_NE(nullptr, nsn);
+  ASSERT_EQ(non_primary, *nsn);
+  ASSERT_TRUE(nsn->copied);
+  ASSERT_EQ(nsn->copy_progress, 10);
+
   ASSERT_EQ(0, snapshot_remove(&ioctx, oid, 1));
   ASSERT_EQ(0, snapshot_remove(&ioctx, oid, 2));
 }

sunilangadi2/ceph-ci

(cherry picked from commit a27fa5fb51ff11dde39ea77a00428c4120162160)
Resolves: rhbz#1860073

src/test/cls_rbd/test_cls_rbd.cc
@@ -2207,6 +2207,15 @@ TEST_F(TestClsRbd, mirror_snapshot) {
   ASSERT_FALSE(nsn->copied);
   ASSERT_EQ(nsn->last_copied_object_number, 0);
 
+  ASSERT_EQ(0, mirror_image_snapshot_set_copy_progress(&ioctx, oid, 2, true,
+                                                       10));
+  ASSERT_EQ(0, snapshot_get(&ioctx, oid, 2, &snap));
+  nsn = boost::get<cls::rbd::MirrorNonPrimarySnapshotNamespace>(
+    &snap.snapshot_namespace);
+  ASSERT_NE(nullptr, nsn);
+  ASSERT_TRUE(nsn->copied);
+  ASSERT_EQ(nsn->last_copied_object_number, 10);
+
   ASSERT_EQ(0, snapshot_remove(&ioctx, oid, 1));
   ASSERT_EQ(0, snapshot_remove(&ioctx, oid, 2));
 }
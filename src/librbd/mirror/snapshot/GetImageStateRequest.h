trociny/ceph

librbd: requests for mirror snapshot image state
Signed-off-by: Mykola Golub <mgolub@suse.com>

src/librbd/mirror_snapshot/GetImageStateRequest.h
@@ -1,0 +1,74 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H
+
+#include "include/buffer.h"
+#include "include/types.h"
+
+struct Context;
+
+namespace librbd {
+
+struct ImageCtx;
+
+namespace mirror_snapshot {
+
+struct ImageState;
+
+template <typename ImageCtxT = librbd::ImageCtx>
+class GetImageStateRequest {
+public:
+  static GetImageStateRequest *create(ImageCtxT *image_ctx, uint64_t snap_id,
+                                      ImageState *image_state,
+                                      Context *on_finish) {
+    return new GetImageStateRequest(image_ctx, snap_id, image_state, on_finish);
+  }
+
+  GetImageStateRequest(ImageCtxT *image_ctx, uint64_t snap_id,
+                       ImageState *image_state, Context *on_finish)
+    : m_image_ctx(image_ctx), m_snap_id(snap_id), m_image_state(image_state),
+      m_on_finish(on_finish) {
+  }
+
+  void send();
+
+private:
+  /**
+   * @verbatim
+   *
+   * <start>
+   *    |
+   *    v
+   * READ_OBJECT (repeat for
+   *    |         every object)
+   *    v
+   * <finish>
+   *
+   * @endverbatim
+   */
+
+  ImageCtxT *m_image_ctx;
+  uint64_t m_snap_id;
+  ImageState *m_image_state;
+  Context *m_on_finish;
+
+  bufferlist m_bl;
+  bufferlist m_state_bl;
+
+  size_t m_object_count = 0;
+  size_t m_object_index = 0;
+
+  void read_object();
+  void handle_read_object(int r);
+
+  void finish(int r);
+};
+
+} // namespace mirror_snapshot
+} // namespace librbd
+
+extern template class librbd::mirror_snapshot::GetImageStateRequest<librbd::ImageCtx>;
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H

src/librbd/mirror_snapshot/RemoveImageStateRequest.h
@@ -1,0 +1,74 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H
+
+#include "include/buffer.h"
+#include "include/types.h"
+
+struct Context;
+
+namespace librbd {
+
+struct ImageCtx;
+
+namespace mirror_snapshot {
+
+template <typename ImageCtxT = librbd::ImageCtx>
+class RemoveImageStateRequest {
+public:
+  static RemoveImageStateRequest *create(ImageCtxT *image_ctx, uint64_t snap_id,
+                                         Context *on_finish) {
+      return new RemoveImageStateRequest(image_ctx, snap_id, on_finish);
+  }
+
+  RemoveImageStateRequest(ImageCtxT *image_ctx, uint64_t snap_id,
+                          Context *on_finish)
+    : m_image_ctx(image_ctx), m_snap_id(snap_id), m_on_finish(on_finish) {
+  }
+
+  void send();
+
+private:
+  /**
+   * @verbatim
+   *
+   * <start>
+   *    |
+   *    v
+   * GET_OBJECT_COUNT
+   *    |
+   *    v
+   * REMOVE_OBJECT (repeat for
+   *    |           every object)
+   *    v
+   * <finish>
+   *
+   * @endverbatim
+   */
+
+  ImageCtxT *m_image_ctx;
+  uint64_t m_snap_id;
+  Context *m_on_finish;
+
+  bufferlist m_bl;
+
+  size_t m_object_count = 0;
+  size_t m_object_index = 0;
+
+  void get_object_count();
+  void handle_get_object_count(int r);
+
+  void remove_object();
+  void handle_remove_object(int r);
+
+  void finish(int r);
+};
+
+} // namespace mirror_snapshot
+} // namespace librbd
+
+extern template class librbd::mirror_snapshot::RemoveImageStateRequest<librbd::ImageCtx>;
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H

src/librbd/mirror_snapshot/Utils.h
@@ -1,0 +1,26 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H
+
+#include "include/stringify.h"
+
+namespace librbd {
+namespace mirror_snapshot {
+namespace util {
+
+const std::string IMAGE_STATE_OBJECT_PREFIX = "rbd_mirror_snapshot.";
+
+template <typename ImageCtxT = librbd::ImageCtx>
+std::string image_state_object_name(ImageCtxT *image_ctx, uint64_t snap_id,
+                                    uint64_t index) {
+  return IMAGE_STATE_OBJECT_PREFIX + image_ctx->id + "." +
+    stringify(snap_id) + "." + stringify(index);
+}
+
+} // namespace util
+} // namespace mirror_snapshot
+} // namespace librbd
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H

sunilangadi2/ceph-ci

(cherry picked from commit c6feb3411ff3c0bf2044d06b65dd738e002326db)
Resolves: rhbz#1860073

src/librbd/mirror/snapshot/GetImageStateRequest.h
@@ -1,0 +1,76 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H
+
+#include "include/buffer.h"
+#include "include/types.h"
+
+struct Context;
+
+namespace librbd {
+
+struct ImageCtx;
+
+namespace mirror {
+namespace snapshot {
+
+struct ImageState;
+
+template <typename ImageCtxT = librbd::ImageCtx>
+class GetImageStateRequest {
+public:
+  static GetImageStateRequest *create(ImageCtxT *image_ctx, uint64_t snap_id,
+                                      ImageState *image_state,
+                                      Context *on_finish) {
+    return new GetImageStateRequest(image_ctx, snap_id, image_state, on_finish);
+  }
+
+  GetImageStateRequest(ImageCtxT *image_ctx, uint64_t snap_id,
+                       ImageState *image_state, Context *on_finish)
+    : m_image_ctx(image_ctx), m_snap_id(snap_id), m_image_state(image_state),
+      m_on_finish(on_finish) {
+  }
+
+  void send();
+
+private:
+  /**
+   * @verbatim
+   *
+   * <start>
+   *    |
+   *    v
+   * READ_OBJECT (repeat for
+   *    |         every object)
+   *    v
+   * <finish>
+   *
+   * @endverbatim
+   */
+
+  ImageCtxT *m_image_ctx;
+  uint64_t m_snap_id;
+  ImageState *m_image_state;
+  Context *m_on_finish;
+
+  bufferlist m_bl;
+  bufferlist m_state_bl;
+
+  size_t m_object_count = 0;
+  size_t m_object_index = 0;
+
+  void read_object();
+  void handle_read_object(int r);
+
+  void finish(int r);
+};
+
+} // namespace snapshot
+} // namespace mirror
+} // namespace librbd
+
+extern template class librbd::mirror::snapshot::GetImageStateRequest<librbd::ImageCtx>;
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_GET_IMAGE_STATE_REQUEST_H

src/librbd/mirror/snapshot/RemoveImageStateRequest.h
@@ -1,0 +1,75 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H
+
+#include "include/buffer.h"
+#include "include/types.h"
+
+struct Context;
+
+namespace librbd {
+
+struct ImageCtx;
+
+namespace mirror {
+namespace snapshot {
+
+template <typename ImageCtxT = librbd::ImageCtx>
+class RemoveImageStateRequest {
+public:
+  static RemoveImageStateRequest *create(ImageCtxT *image_ctx, uint64_t snap_id,
+                                         Context *on_finish) {
+      return new RemoveImageStateRequest(image_ctx, snap_id, on_finish);
+  }
+
+  RemoveImageStateRequest(ImageCtxT *image_ctx, uint64_t snap_id,
+                          Context *on_finish)
+    : m_image_ctx(image_ctx), m_snap_id(snap_id), m_on_finish(on_finish) {
+  }
+
+  void send();
+
+private:
+  /**
+   * @verbatim
+   *
+   * <start>
+   *    |
+   *    v
+   * GET_OBJECT_COUNT
+   *    |
+   *    v
+   * REMOVE_OBJECT (repeat for
+   *    |           every object)
+   *    v
+   * <finish>
+   *
+   * @endverbatim
+   */
+
+  ImageCtxT *m_image_ctx;
+  uint64_t m_snap_id;
+  Context *m_on_finish;
+
+  bufferlist m_bl;
+
+  size_t m_object_count = 0;
+
+  void get_object_count();
+  void handle_get_object_count(int r);
+
+  void remove_object();
+  void handle_remove_object(int r);
+
+  void finish(int r);
+};
+
+} // namespace snapshot
+} // namespace mirror
+} // namespace librbd
+
+extern template class librbd::mirror::snapshot::RemoveImageStateRequest<librbd::ImageCtx>;
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_REMOVE_IMAGE_STATE_REQUEST_H

src/librbd/mirror/snapshot/Utils.h
@@ -1,0 +1,28 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H
+#define CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H
+
+#include "include/stringify.h"
+
+namespace librbd {
+namespace mirror {
+namespace snapshot {
+namespace util {
+
+const std::string IMAGE_STATE_OBJECT_PREFIX = "rbd_mirror_snapshot.";
+
+template <typename ImageCtxT = librbd::ImageCtx>
+std::string image_state_object_name(ImageCtxT *image_ctx, uint64_t snap_id,
+                                    uint64_t index) {
+  return IMAGE_STATE_OBJECT_PREFIX + image_ctx->id + "." +
+    stringify(snap_id) + "." + stringify(index);
+}
+
+} // namespace util
+} // namespace snapshot
+} // namespace mirror
+} // namespace librbd
+
+#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_UTILS_H
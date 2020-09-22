ceph/ceph

mgr/dashboard: fix pool usage calculation
Currently Dashboard Pool usage calculation does not match the output of
'ceph df' command.

Fixes: https://tracker.ceph.com/issues/45185
Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
@@ -12,12 +12,14 @@ export class UsageBarComponent implements OnChanges {
   used: number;
   @Input()
   isBinary = true;
+  @Input()
+  decimals = 0;
 
   usedPercentage: number;
   freePercentage: number;
 
   ngOnChanges() {
-    this.usedPercentage = this.total > 0 ? Math.round((this.used / this.total) * 100) : 0;
+    this.usedPercentage = this.total > 0 ? (this.used / this.total) * 100 : 0;
     this.freePercentage = 100 - this.usedPercentage;
   }
 }

sunilangadi2/ceph-ci

(cherry picked from commit b4a9dc17a3de90379964443d26b29f1759824f28)
Conflicts:
	qa/tasks/mgr/dashboard/helper.py
	src/mon/PGMap.cc
	src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-list/pool-list.component.html
	src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-list/pool-list.component.spec.ts
	src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-list/pool-list.component.ts
	src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts:
        - Keep UsageBar component totalBytes/usedBytes names
        - Bring new UsageBar option decimal
(cherry picked from commit cc2072312f35e0a2edc1c9d57f9c2b555be49ca4)
Resolves: rhbz#1826690

src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
@@ -10,6 +10,8 @@ export class UsageBarComponent implements OnChanges {
   totalBytes: number;
   @Input()
   usedBytes: number;
+  @Input()
+  decimals = 0;
 
   usedPercentage: number;
   freePercentage: number;
@@ -18,7 +20,7 @@ export class UsageBarComponent implements OnChanges {
   constructor() {}
 
   ngOnChanges() {
-    this.usedPercentage = Math.round((this.usedBytes / this.totalBytes) * 100);
+    this.usedPercentage = this.totalBytes > 0 ? (this.usedBytes / this.totalBytes) * 100 : 0;
     this.freePercentage = 100 - this.usedPercentage;
     this.freeBytes = this.totalBytes - this.usedBytes;
   }
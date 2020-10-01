82573dee52998864cae411f334e3cd0e4d5330ce
Sidharth Anupkrishnan <sanupkri@redhat.com>
2020-10-01 21:04:56 +0530

    mds: Optimize Balancer code path to use newer config fetch and also cache the fetched values when there is a hot code path
    This change is necessary because the new way of setting config is to use the ceph config command or the asok interfave rahter than the old way which involved editing the ceph.conf and restarting the daemons to reflect the changes. Have updated the code to support runtime config changes.
    Signed-off-by: Sidharth Anupkrishnan <sanupkri@redhat.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -262,6 +262,7 @@ Server::Server(MDSRank *m, MetricsHandler *metrics_handler) :
   cap_acquisition_throttle = g_conf().get_val<uint64_t>("mds_session_cap_acquisition_throttle");
   max_caps_throttle_ratio = g_conf().get_val<double>("mds_session_max_caps_throttle_ratio");
   caps_throttle_retry_request_timeout = g_conf().get_val<double>("mds_cap_acquisition_throttle_retry_request_timeout");
+  bal_fragment_size_max = g_conf().get_val<int64_t>("mds_bal_fragment_size_max");
   supported_features = feature_bitset_t(CEPHFS_FEATURES_MDS_SUPPORTED);
 }
 
@@ -1229,6 +1230,7 @@ void Server::handle_conf_change(const std::set<std::string>& changed) {
   if (changed.count("mds_client_delegate_inos_pct")) {
     delegate_inos_pct = g_conf().get_val<uint64_t>("mds_client_delegate_inos_pct");
   }
+<<<<<<< HEAD
   if (changed.count("mds_max_caps_per_client")) {
     max_caps_per_client = g_conf().get_val<uint64_t>("mds_max_caps_per_client");
   }
@@ -1240,6 +1242,10 @@ void Server::handle_conf_change(const std::set<std::string>& changed) {
   }
   if (changed.count("mds_cap_acquisition_throttle_retry_request_timeout")) {
     caps_throttle_retry_request_timeout = g_conf().get_val<double>("mds_cap_acquisition_throttle_retry_request_timeout");
+=======
+  if (changed.count("mds_bal_fragment_size_max")) {
+    bal_fragment_size_max = g_conf().get_val<int64_t>("mds_bal_fragment_size_max");
+>>>>>>> 6eaeb39e10... mds: Optimize Balancer code path to use newer config fetch and also cache the fetched values when there is a hot code path
   }
 }
 
@@ -3180,8 +3186,8 @@ bool Server::check_access(MDRequestRef& mdr, CInode *in, unsigned mask)
 bool Server::check_fragment_space(MDRequestRef &mdr, CDir *in)
 {
   const auto size = in->get_frag_size();
-  if (size >= g_conf()->mds_bal_fragment_size_max) {
-    dout(10) << "fragment " << *in << " size exceeds " << g_conf()->mds_bal_fragment_size_max << " (ENOSPC)" << dendl;
+  if (size >= bal_fragment_size_max) {
+    dout(10) << "fragment " << *in << " size exceeds " << bal_fragment_size_max << " (ENOSPC)" << dendl;
     respond_to_request(mdr, -ENOSPC);
     return false;
   }

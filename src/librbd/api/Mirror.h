c73c4a61b8a9b467e3d830e0928b74b7069c445e
Mykola Golub <mgolub@suse.com>
2020-10-12 10:25:46 +0100

    librbd: async API for creating mirror snapshots
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    (cherry picked from commit d8468ec7e49d93860c3b8b06ff36bde539d10a94)
    
    Conflicts:
    	src/librbd/api/Mirror.h/cc: snapshot flags not implemented
    
    Resolves: rhbz#1893740

--- src/librbd/api/Mirror.h
+++ src/librbd/api/Mirror.h
@@ -114,6 +114,8 @@ struct Mirror {
   static int image_get_instance_id(ImageCtxT *ictx, std::string *instance_id);
 
   static int image_snapshot_create(ImageCtxT *ictx, uint64_t *snap_id);
+  static void image_snapshot_create(ImageCtxT *ictx, uint32_t flags,
+                                    uint64_t *snap_id, Context *on_finish);
 };
 
 } // namespace api

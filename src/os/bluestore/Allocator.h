33dd7451d163bc7ac8a8ff97955dfffae832aa3c
Abutalib Aghayev <agayev@psu.edu>
2020-11-04 15:35:17 -0500

    os/bluestore: Introduce plumbing necessary for garbage collecting zones.
    Signed-off-by: Abutalib Aghayev <agayev@psu.edu>

--- src/os/bluestore/Allocator.h
+++ src/os/bluestore/Allocator.h
@@ -49,7 +49,10 @@ public:
   virtual void dump() = 0;
   virtual void dump(std::function<void(uint64_t offset, uint64_t length)> notify) = 0;
 
-  virtual void set_zone_states(std::vector<zone_state_t> &&_zone_states) {}
+  virtual void zoned_set_zone_states(std::vector<zone_state_t> &&_zone_states) {}
+  virtual std::deque<uint64_t> zoned_get_zones_to_clean() { return {}; }
+  virtual bool zoned_needs_cleaning() { return false; }
+
   virtual void init_add_free(uint64_t offset, uint64_t length) = 0;
   virtual void init_rm_free(uint64_t offset, uint64_t length) = 0;
 

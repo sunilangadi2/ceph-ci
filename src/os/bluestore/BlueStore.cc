eb2217e53c5c6cb88532252b8d2740c000367799
PMemDev <ifedotov@suse.com>
2020-12-07 18:32:48 +0000

    os/bluestore: introduce "truncate" latency perf counter.
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5017,6 +5017,8 @@ void BlueStore::_init_logger()
     "Average collection listing latency");
   b.add_time_avg(l_bluestore_remove_lat, "remove_lat",
     "Average removal latency");
+  b.add_time_avg(l_bluestore_truncate_lat, "truncate_lat",
+    "Average truncate latency");
 
   logger = b.create_perf_counters();
   cct->get_perfcounters_collection()->add(logger);
@@ -14686,12 +14688,27 @@ int BlueStore::_truncate(TransContext *txc,
 	   << " 0x" << std::hex << offset << std::dec
            << " reclaim " << reclaim_mode
 	   << dendl;
+
+  auto start_time = mono_clock::now();
   int r = 0;
   if (offset >= OBJECT_MAX_SIZE) {
     r = -E2BIG;
   } else {
     _do_truncate(txc, c, o, offset, nullptr, reclaim_mode);
   }
+  log_latency_fn(
+    __func__,
+    l_bluestore_truncate_lat,
+    mono_clock::now() - start_time,
+    cct->_conf->bluestore_log_op_age,
+    [&](const ceph::timespan& lat) {
+      ostringstream ostr;
+      ostr << ", lat = " << timespan_str(lat)
+        << " cid =" << c->cid
+        << " oid =" << o->oid;
+      return ostr.str();
+    }
+  );
   dout(10) << __func__ << " " << c->cid << " " << o->oid
 	   << " 0x" << std::hex << offset << std::dec
 	   << " = " << r << dendl;
@@ -14806,9 +14823,9 @@ int BlueStore::_remove(TransContext *txc,
   dout(15) << __func__ << " " << c->cid << " " << o->oid
 	   << " onode " << o.get()
 	   << " txc "<< txc << dendl;
-
-  auto start_time = mono_clock::now();
+ auto start_time = mono_clock::now();
   int r = _do_remove(txc, c, o);
+
   log_latency_fn(
     __func__,
     l_bluestore_remove_lat,

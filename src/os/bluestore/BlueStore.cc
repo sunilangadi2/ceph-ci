ifed01/ceph

os/bluestore: allow onode trim on unpin.
Additionally enables immediate trim for non-existent objects to avoid
cache pollution.

Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/os/bluestore/BlueStore.cc
@@ -824,12 +824,25 @@ struct LruOnodeCacheShard : public BlueStore::OnodeCacheShard {
     ++num_pinned;
     dout(20) << __func__ << this << " " << " " << " " << o->oid << " pinned" << dendl;
   }
-  void _unpin(BlueStore::Onode* o) override
+  void _unpin(BlueStore::Onode* o, bool trim) override
   {
-    lru.push_front(*o);
     ceph_assert(num_pinned);
     --num_pinned;
-    dout(20) << __func__ << this << " " << " " << " " << o->oid << " unpinned" << dendl;
+    if (!trim) {
+      lru.push_front(*o);
+      dout(20) << __func__ << this << " " << " " << " " << o->oid
+               << " unpinned "
+               << dendl;
+    } else {
+      auto unpinned = o->pop_cache();
+      ceph_assert(unpinned);
+      --num;
+      dout(20) << __func__ << this << " " << " " << " " << o->oid
+               << " unpinned and trimmed"
+               << dendl;
+      o->c->onode_map._remove(o->oid);
+      // onode might be removed beyond this point
+    }
   }
 
   void _trim_to(uint64_t new_size) override
@@ -1685,10 +1698,13 @@ bool BlueStore::OnodeSpace::map_any(std::function<bool(OnodeRef)> f)
 {
   std::lock_guard l(cache->lock);
   ldout(cache->cct, 20) << __func__ << dendl;
-  for (auto& i : onode_map) {
-    if (f(i.second)) {
+  auto i = onode_map.begin();
+  while (i != onode_map.end()) {
+    OnodeRef o = i->second;
+    if (f(o)) {
       return true;
     }
+    ++i;
   }
   return false;
 }
@@ -3264,20 +3280,25 @@ void BlueStore::Onode::get() {
   }
 }
 void BlueStore::Onode::put() {
-  if (--nref == 2) {
+  int nref_local = --nref;
+  if (nref_local == 2) {
     c->get_onode_cache()->unpin(this, [&]() {
         bool was_pinned = pinned;
         pinned = pinned && nref > 2; // intentionally use > not >= as we have
                                      // +1 due to pinned state
-        bool r = was_pinned && !pinned;
+        bool new_unpin = was_pinned && !pinned;
+        auto r = !cached || !new_unpin ?
+          OnodeCacheShard::UNPIN_SKIP :
+          exists ?
+            OnodeCacheShard::UNPIN_PROCEED_CACHING :
+              OnodeCacheShard::UNPIN_PROCEED_TRIM;
         // additional decrement for newly unpinned instance
-        if (r) {
-          --nref;
-        }
-        return cached && r;
+        if (new_unpin)
+          nref_local = --nref; // after this point Onode might be removed
+        return r;
       });
   }
-  if (nref == 0) {
+  if (nref_local == 0) {
     delete this;
   }
 }

src/os/bluestore/BlueStore.h
@@ -1220,7 +1220,7 @@ public:
     std::array<std::pair<ghobject_t, ceph::mono_clock::time_point>, 64> dumped_onodes;
 
     virtual void _pin(Onode* o) = 0;
-    virtual void _unpin(Onode* o) = 0;
+    virtual void _unpin(Onode* o, bool cache) = 0;
 
   public:
     OnodeCacheShard(CephContext* cct) : CacheShard(cct) {}
@@ -1236,10 +1236,16 @@ public:
       }
     }
 
-    void unpin(Onode* o, std::function<bool()> validator) {
+    enum {
+      UNPIN_SKIP,
+      UNPIN_PROCEED_CACHING,
+      UNPIN_PROCEED_TRIM,
+    };
+    void unpin(Onode* o, std::function<int()> validator) {
       std::lock_guard l(lock);
-      if (validator()) {
-        _unpin(o);
+      int r = validator();
+      if (r == UNPIN_PROCEED_CACHING || r == UNPIN_PROCEED_TRIM) {
+        _unpin(o, r == UNPIN_PROCEED_TRIM);
       }
     }
 

sunilangadi2/ceph-ci

src/os/bluestore/BlueStore.cc
@@ -1067,12 +1067,25 @@ struct LruOnodeCacheShard : public BlueStore::OnodeCacheShard {
     ++num_pinned;
     dout(20) << __func__ << this << " " << " " << " " << o->oid << " pinned" << dendl;
   }
-  void _unpin(BlueStore::Onode* o) override
+  void _unpin(BlueStore::Onode* o, bool trim) override
   {
-    lru.push_front(*o);
     ceph_assert(num_pinned);
     --num_pinned;
-    dout(20) << __func__ << this << " " << " " << " " << o->oid << " unpinned" << dendl;
+    if (!trim) {
+      lru.push_front(*o);
+      dout(20) << __func__ << this << " " << " " << " " << o->oid
+               << " unpinned "
+               << dendl;
+    } else {
+      auto unpinned = o->pop_cache();
+      ceph_assert(unpinned);
+      --num;
+      dout(20) << __func__ << this << " " << " " << " " << o->oid
+               << " unpinned and trimmed"
+               << dendl;
+      o->c->onode_map._remove(o->oid);
+      // onode might be removed beyond this point
+    }
   }
 
   void _trim_to(uint64_t new_size) override
@@ -1932,10 +1945,13 @@ bool BlueStore::OnodeSpace::map_any(std::function<bool(OnodeRef)> f)
 {
   std::lock_guard l(cache->lock);
   ldout(cache->cct, 20) << __func__ << dendl;
-  for (auto& i : onode_map) {
-    if (f(i.second)) {
+  auto i = onode_map.begin();
+  while (i != onode_map.end()) {
+    OnodeRef o = i->second; // ++ref count to make sure i isn't invalidated
+    if (f(o)) {
       return true;
     }
+    ++i;
   }
   return false;
 }
@@ -3517,12 +3533,16 @@ void BlueStore::Onode::put() {
         bool was_pinned = pinned;
         pinned = pinned && nref > 2; // intentionally use > not >= as we have
                                      // +1 due to pinned state
-        bool r = was_pinned && !pinned;
+        bool new_unpin = was_pinned && !pinned;
+        auto r = !cached || !new_unpin ?
+          OnodeCacheShard::UNPIN_SKIP :
+          exists ?
+            OnodeCacheShard::UNPIN_PROCEED_CACHING :
+            OnodeCacheShard::UNPIN_PROCEED_TRIM;
         // additional decrement for newly unpinned instance
-        if (r) {
-          n = --nref;
-        }
-        return cached && r;
+        if (new_unpin)
+          n = --nref; // after this point Onode might be removed
+        return r;
       });
   }
   if (n == 0) {

src/os/bluestore/BlueStore.h
@@ -1226,7 +1226,7 @@ public:
     std::array<std::pair<ghobject_t, ceph::mono_clock::time_point>, 64> dumped_onodes;
 
     virtual void _pin(Onode* o) = 0;
-    virtual void _unpin(Onode* o) = 0;
+    virtual void _unpin(Onode* o, bool trim) = 0;
 
   public:
     OnodeCacheShard(CephContext* cct) : CacheShard(cct) {}
@@ -1242,10 +1242,16 @@ public:
       }
     }
 
-    void unpin(Onode* o, std::function<bool()> validator) {
+    enum {
+      UNPIN_SKIP,
+      UNPIN_PROCEED_CACHING,
+      UNPIN_PROCEED_TRIM,
+    };
+    void unpin(Onode* o, std::function<int()> validator) {
       std::lock_guard l(lock);
-      if (validator()) {
-        _unpin(o);
+      int r = validator();
+      if (r == UNPIN_PROCEED_CACHING || r == UNPIN_PROCEED_TRIM) {
+        _unpin(o, r == UNPIN_PROCEED_TRIM);
       }
     }
 
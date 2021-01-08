c4fe4a1ba89e7085bfed1f9e5e2abca0855884fa
Neha Ojha <nojha@redhat.com>
2021-01-08 20:12:28 +0000

    Revert "osd: Extracting scrub code from PrimaryLogPG (a derivative of 'PG')"
    This reverts commit 5f6a9015acc6a554c903928fd4b987b75e2f3c73.

--- src/osd/pg_scrubber.h
+++ src/osd/pg_scrubber.h
@@ -674,4 +674,7 @@ class PgScrubber : public ScrubPgIF, public ScrubMachineListener {
   };
 
   preemption_data_t preemption_data;
+
+  // debug/development temporary code:
+  void debug_dump_reservations(std::string_view header_txt) const;
 };

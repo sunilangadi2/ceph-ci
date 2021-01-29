ee5de9840960ea140e3313744b97844c340f085d
Sebastian Wagner <sebastian.wagner@suse.com>
2021-01-29 18:49:25 +0100

    mgr/cephadm: purge service: remove `mds_join_fs`
    Also add a unit test
    
    Fixes: https://tracker.ceph.com/issues/48597
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/cephadm/services/cephadmservice.py
+++ src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -568,6 +568,13 @@ class MdsService(CephService):
         # if no mds found, return empty Daemon Desc
         return DaemonDescription()
 
+    def purge(self, service_name: str) -> None:
+        self.mgr.check_mon_command({
+            'prefix': 'config rm',
+            'who': service_name,
+            'name': 'mds_join_fs',
+        })
+
 
 class RgwService(CephService):
     TYPE = 'rgw'

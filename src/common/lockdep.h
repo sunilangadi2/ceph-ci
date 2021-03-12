e6dda988a84e77f9fc3f301e93b0d9c7ff3844d9
Kefu Chai <kchai@redhat.com>
2021-03-12 21:12:08 +0800

    common/lockdep: disable lockdep if !CEPH_DEBUG_MUTEX
    make lockdep a feature which can only be enabled at compile time.
    
    we don't need to have lockdep when CEPH_DEBUG_MUTEX is OFF, so
    just comment it out in Release builds.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/lockdep.h
+++ src/common/lockdep.h
@@ -16,6 +16,9 @@
 #define CEPH_LOCKDEP_H
 
 #include "include/common_fwd.h"
+
+#ifdef CEPH_DEBUG_MUTEX
+
 extern bool g_lockdep;
 
 extern void lockdep_register_ceph_context(CephContext *cct);
@@ -30,4 +33,17 @@ extern int lockdep_will_lock(const char *n, int id, bool force_backtrace=false,
 extern int lockdep_locked(const char *n, int id, bool force_backtrace=false);
 extern int lockdep_will_unlock(const char *n, int id);
 extern int lockdep_dump_locks();
+
+#else
+
+#define g_lockdep false
+#define lockdep_will_lock(n_, i_) i_
+#define lockdep_will_unlock(n_, i_) i_
+#define lockdep_locked(n_, i_) i_
+#define lockdep_register(n_) 0
+#define lockdep_unregister(i_)
+
+#endif	// CEPH_DEBUG_MUTEX
+
+
 #endif

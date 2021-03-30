c886235902f8071f666564a1ba117a50aafbb406
Deepika Upadhyay <dupadhya@redhat.com>
2021-03-30 13:10:40 +0530

    osd/PeeringState: remove rollingforwarding from activate
    we always ensure rollingforward during log addition.
    rollforwarding when we go active, hence seems redundant and in this case
    was updating can_rollback_to without ensuring we have enough copies.
    
    fixes: https://tracker.ceph.com/issues/48613
    
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

--- src/osd/PeeringState.h
+++ src/osd/PeeringState.h
@@ -2314,8 +2314,8 @@ public:
    * applicable stretch cluster constraints.
    */
   bool acting_set_writeable() {
-    return (acting.size() >= pool.info.min_size) &&
-      (pool.info.stretch_set_can_peer(acting, *get_osdmap(), NULL));
+    return (acting.size() >= pool.info.min_size)
+      && (pool.info.stretch_set_can_peer(acting, *get_osdmap(), NULL));
   }
 
   /**

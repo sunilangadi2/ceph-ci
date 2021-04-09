4bf9d7e31018e36c68ea8bbea54d8904a27f07c2
Sage Weil <sage@newdream.net>
2021-04-09 14:43:45 -0400

    mgr/cephadm/schedule: add per_host_sidecar support
    This will be used to schedule a per-host keepalived alongside other
    services.
    
    Implement this as a final stage for place() that puts one per host and
    also takes existing/stray daemons into consideration.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/pybind/mgr/cephadm/schedule.py
+++ src/pybind/mgr/cephadm/schedule.py
@@ -79,6 +79,7 @@ class HostAssignment(object):
         self.daemons = daemons
         self.networks = networks
         self.allow_colo = allow_colo
+        self.per_host_sidecar = per_host_sidecar
         self.ports_start = spec.get_port_start()
 
     def hosts_by_label(self, label: str) -> List[orchestrator.HostSpec]:
@@ -123,6 +124,35 @@ class HostAssignment(object):
                     f'Cannot place {self.spec.one_line_str()}: No matching '
                     f'hosts for label {self.spec.placement.label}')
 
+    def place_sidecars(
+            self,
+            slots: List[DaemonPlacement],
+            to_add: List[DaemonPlacement],
+            to_remove: List[orchestrator.DaemonDescription],
+    ) -> Tuple[List[DaemonPlacement], List[DaemonPlacement], List[orchestrator.DaemonDescription]]:
+        if self.per_host_sidecar:
+            sidecar_slots = [
+                DaemonPlacement(daemon_type=self.per_host_sidecar,
+                                hostname=hostname)
+                for hostname in set([s.hostname for s in slots])
+            ]
+            existing_sidecars = [
+                d for d in self.daemons if d.daemon_type == self.per_host_sidecar
+            ]
+            slots += sidecar_slots
+            for dd in existing_sidecars:
+                found = False
+                for p in sidecar_slots:
+                    if p.matches_daemon(dd):
+                        sidecar_slots.remove(p)
+                        found = True
+                        break
+                if not found:
+                    to_remove.append(dd)
+            to_add += sidecar_slots
+
+        return slots, to_add, to_remove
+
     def place(self):
         # type: () -> Tuple[List[DaemonPlacement], List[DaemonPlacement], List[orchestrator.DaemonDescription]]
         """
@@ -158,11 +188,11 @@ class HostAssignment(object):
             per_host = 1 + ((count - 1) // len(candidates))
             candidates = expand_candidates(candidates, per_host)
 
-        # consider active daemons first
+        # consider active (primary) daemons first
         daemons = [
-            d for d in self.daemons if d.is_active
+            d for d in self.daemons if d.is_active and d.daemon_type == self.primary_daemon_type
         ] + [
-            d for d in self.daemons if not d.is_active
+            d for d in self.daemons if not d.is_active and d.daemon_type == self.primary_daemon_type
         ]
 
         # sort candidates into existing/used slots that already have a
@@ -192,7 +222,7 @@ class HostAssignment(object):
         # If we don't have <count> the list of candidates is definitive.
         if count is None:
             logger.debug('Provided hosts: %s' % candidates)
-            return candidates, others, to_remove
+            return self.place_sidecars(candidates, others, to_remove)
 
         # The number of new slots that need to be selected in order to fulfill count
         need = count - len(existing)
@@ -201,13 +231,13 @@ class HostAssignment(object):
         if need <= 0:
             to_remove.extend(existing[count:])
             del existing_slots[count:]
-            return existing_slots, [], to_remove
+            return self.place_sidecars(existing_slots, [], to_remove)
 
         # ask the scheduler to select additional slots
         to_add = others[:need]
         logger.debug('Combine hosts with existing daemons %s + new hosts %s' % (
             existing, to_add))
-        return existing_slots + to_add, to_add, to_remove
+        return self.place_sidecars(existing_slots + to_add, to_add, to_remove)
 
     def find_ip_on_host(self, hostname: str, subnets: List[str]) -> Optional[str]:
         for subnet in subnets:

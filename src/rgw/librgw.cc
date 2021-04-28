alimaredia/ceph

rgw: make RGWLib a DoutPrefixProvider
Signed-off-by: Ali Maredia <amaredia@redhat.com>

src/rgw/librgw.cc
@@ -61,7 +61,6 @@
 #include <string>
 #include <mutex>
 
-
 #define dout_subsys ceph_subsys_rgw
 
 bool global_stop = false;
@@ -535,8 +534,7 @@ namespace rgw {
       g_conf()->rgw_run_sync_thread &&
       g_conf()->rgw_nfs_run_sync_thread;
 
-    const DoutPrefix dp(cct.get(), dout_subsys, "librgw: ");
-    store = RGWStoreManager::get_storage(&dp, g_ceph_context,
+    store = RGWStoreManager::get_storage(this, g_ceph_context,
 					 "rados",
 					 run_gc,
 					 run_lc,
@@ -579,7 +577,7 @@ namespace rgw {
     ldh->init();
     ldh->bind();
 
-    rgw_log_usage_init(&dp, g_ceph_context, store);
+    rgw_log_usage_init(this, g_ceph_context, store);
 
     // XXX ex-RGWRESTMgr_lib, mgr->set_logging(true)
 
@@ -603,7 +601,7 @@ namespace rgw {
     service_map_meta["frontend_type#" + fe_count] = "rgw-nfs";
     service_map_meta["frontend_config#" + fe_count] = fec->get_config();
 
-    fe->init(&dp);
+    fe->init(this);
     if (r < 0) {
       derr << "ERROR: failed initializing frontend" << dendl;
       return r;
@@ -670,11 +668,10 @@ namespace rgw {
 
   int RGWLibRequest::read_permissions(RGWOp* op, optional_yield y) {
     /* bucket and object ops */
-    const DoutPrefix dp(store->ctx(), dout_subsys, "librgw: ");
     int ret =
-      rgw_build_bucket_policies(&dp, rgwlib.get_store(), get_state(), y);
+      rgw_build_bucket_policies(op, rgwlib.get_store(), get_state(), y);
     if (ret < 0) {
-      ldpp_dout(&dp, 10) << "read_permissions (bucket policy) on "
+      ldpp_dout(op, 10) << "read_permissions (bucket policy) on "
 				  << get_state()->bucket << ":"
 				  << get_state()->object
 				  << " only_bucket=" << only_bucket()
@@ -683,10 +680,10 @@ namespace rgw {
 	ret = -EACCES;
     } else if (! only_bucket()) {
       /* object ops */
-      ret = rgw_build_object_policies(&dp, rgwlib.get_store(), get_state(),
+      ret = rgw_build_object_policies(op, rgwlib.get_store(), get_state(),
 				      op->prefetch_data(), y);
       if (ret < 0) {
-	ldpp_dout(&dp, 10) << "read_permissions (object policy) on"
+	ldpp_dout(op, 10) << "read_permissions (object policy) on"
 				    << get_state()->bucket << ":"
 				    << get_state()->object
 				    << " ret=" << ret << dendl;

sunilangadi2/ceph-ci

src/rgw/librgw.cc
@@ -67,7 +67,6 @@
 #include <string>
 #include <mutex>
 
-
 #define dout_subsys ceph_subsys_rgw
 
 bool global_stop = false;
@@ -541,8 +540,7 @@ namespace rgw {
       g_conf()->rgw_run_sync_thread &&
       g_conf()->rgw_nfs_run_sync_thread;
 
-    const DoutPrefix dp(cct.get(), dout_subsys, "librgw: ");
-    store = StoreManager::get_storage(&dp, g_ceph_context,
+    store = StoreManager::get_storage(this, g_ceph_context,
 					 "rados",
 					 run_gc,
 					 run_lc,
@@ -585,7 +583,7 @@ namespace rgw {
     ldh->init();
     ldh->bind();
 
-    rgw_log_usage_init(&dp, g_ceph_context, store);
+    rgw_log_usage_init(this, g_ceph_context, store);
 
     // XXX ex-RGWRESTMgr_lib, mgr->set_logging(true)
 
@@ -693,11 +691,10 @@ namespace rgw {
 
   int RGWLibRequest::read_permissions(RGWOp* op, optional_yield y) {
     /* bucket and object ops */
-    const DoutPrefix dp(store->ctx(), dout_subsys, "librgw: ");
     int ret =
-      rgw_build_bucket_policies(&dp, rgwlib.get_store(), get_state(), y);
+      rgw_build_bucket_policies(op, rgwlib.get_store(), get_state(), y);
     if (ret < 0) {
-      ldpp_dout(&dp, 10) << "read_permissions (bucket policy) on "
+      ldpp_dout(op, 10) << "read_permissions (bucket policy) on "
 				  << get_state()->bucket << ":"
 				  << get_state()->object
 				  << " only_bucket=" << only_bucket()
@@ -706,10 +703,10 @@ namespace rgw {
 	ret = -EACCES;
     } else if (! only_bucket()) {
       /* object ops */
-      ret = rgw_build_object_policies(&dp, rgwlib.get_store(), get_state(),
+      ret = rgw_build_object_policies(op, rgwlib.get_store(), get_state(),
 				      op->prefetch_data(), y);
       if (ret < 0) {
-	ldpp_dout(&dp, 10) << "read_permissions (object policy) on"
+	ldpp_dout(op, 10) << "read_permissions (object policy) on"
 				    << get_state()->bucket << ":"
 				    << get_state()->object
 				    << " ret=" << ret << dendl;
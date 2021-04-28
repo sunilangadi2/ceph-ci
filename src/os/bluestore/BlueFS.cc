aaca0e4b907be602dc0a120ece0196024c0d1c40
Gabriel BenHanokh <benhanokh@gmail.com>
2021-04-28 20:37:09 +0300

    reverse PR 34109 (Optimizing the lock of bluestore writing process) which led to a deadlock Signed-off-by: Gabriel Benhanokh <gbenhano@redhat.com>

--- src/os/bluestore/BlueFS.cc
+++ src/os/bluestore/BlueFS.cc
@@ -2817,15 +2817,17 @@ int BlueFS::_flush_range(FileWriter *h, uint64_t offset, uint64_t length)
     uint64_t x_len = std::min(p->length - x_off, length);
     bufferlist t;
     t.substr_of(bl, bloff, x_len);
-    h->lock.lock();
-    lock.unlock();
+    // h->lock.lock();
+    // lock.unlock();
     if (cct->_conf->bluefs_sync_write) {
       bdev[p->bdev]->write(p->offset + x_off, t, buffered, h->write_hint);
     } else {
       bdev[p->bdev]->aio_write(p->offset + x_off, t, h->iocv[p->bdev], buffered, h->write_hint);
     }
-    lock.lock();
-    h->lock.unlock();
+
+    // GBH - reverse locking scheme to prevent deadlock
+    // h->lock.unlock();
+    // lock.lock();
     h->dirty_devs[p->bdev] = true;
     if (p->bdev == BDEV_SLOW) {
       bytes_written_slow += t.length();

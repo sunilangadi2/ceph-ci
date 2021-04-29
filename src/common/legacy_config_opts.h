ceph/ceph

os/bluestore: track kv utilization in kv_sync_thread
Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/common/legacy_config_opts.h
@@ -1080,6 +1080,7 @@ OPTION(bluestore_log_collection_list_age, OPT_DOUBLE)
 OPTION(bluestore_debug_enforce_settings, OPT_STR)
 OPTION(bluestore_collection_list_min_prefetch, OPT_INT)
 OPTION(bluestore_collection_list_prefetch, OPT_INT)
+OPTION(bluestore_kv_sync_util_logging_s, OPT_INT)
 
 OPTION(kstore_max_ops, OPT_U64)
 OPTION(kstore_max_bytes, OPT_U64)

sunilangadi2/ceph-ci

(cherry picked from commit 0504b9554998005307647744ba82ffbe9ad8c703)
 Conflicts:
	src/os/bluestore/BlueStore.cc
 trivial
(cherry picked from commit 4c7f6eaf5588cc828940c4428253d23c706696dc)
Resolves: rhbz#1905431

src/common/legacy_config_opts.h
@@ -1098,6 +1098,7 @@ OPTION(bluestore_log_collection_list_age, OPT_DOUBLE)
 OPTION(bluestore_volume_selection_policy, OPT_STR)
 OPTION(bluestore_volume_selection_reserved_factor, OPT_DOUBLE)
 OPTION(bluestore_volume_selection_reserved, OPT_INT)
+OPTION(bluestore_kv_sync_util_logging_s, OPT_DOUBLE)
 
 OPTION(kstore_max_ops, OPT_U64)
 OPTION(kstore_max_bytes, OPT_U64)
8fc1dfa0ae64077d88633c614ba090109e06045e
Radoslaw Zarzynski <rzarzyns@redhat.com>
2021-05-19 12:28:31 +0000

    common: introduce std::optional-returning variants of cmd_getval().
    Using an output paramter instead of returning is confusing but
    common in pre-C++11 code. Let's modernize `cmd_getval()`.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/cmdparse.h
+++ src/common/cmdparse.h
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <stdexcept>
+#include <optional>
 #include <ostream>
 #include <boost/variant.hpp>
 #include "include/ceph_assert.h"	// boost clobbers this
@@ -77,6 +78,18 @@ bool cmd_getval(const cmdmap_t& cmdmap,
   }
 }
 
+template <typename T>
+std::optional<T> cmd_getval(const cmdmap_t& cmdmap,
+			    std::string_view k)
+{
+  T ret;
+  if (const bool found = cmd_getval(cmdmap, k, ret); found) {
+    return std::make_optional(std::move(ret));
+  } else {
+    return std::nullopt;
+  }
+}
+
 // with default
 
 template <typename T, typename V>
@@ -91,6 +104,19 @@ T cmd_getval(const cmdmap_t& cmdmap, std::string_view k,
     return boost::get<T>(cmdmap.find(k)->second);
   } catch (boost::bad_get&) {
     throw bad_cmd_get(k, cmdmap);
+  }
+}
+
+template <typename T>
+std::optional<T> cmd_getval(const cmdmap_t& cmdmap,
+			    std::string_view k,
+			    const T& defval)
+{
+  T ret;
+  if (const bool found = cmd_getval(cmdmap, k, ret, defval); found) {
+    return std::make_optional(std::move(ret));
+  } else {
+    return std::nullopt;
   }
 }
 

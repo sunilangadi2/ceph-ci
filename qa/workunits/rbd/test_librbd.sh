e9f79f9abbdcd9e5ef59a7363cef91e4bd0ddfcc
Deepika Upadhyay <dupadhya@redhat.com>
2021-05-19 16:19:26 +0530

    qa/rbd: test just EncryptionLUKS2 gtest
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

--- qa/workunits/rbd/test_librbd.sh
+++ qa/workunits/rbd/test_librbd.sh
@@ -2,8 +2,8 @@
 
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
-    --error-exitcode=1 ceph_test_librbd
+    --error-exitcode=1 ceph_test_librbd --gtest_filter=*TestLibRBD.TestEncryptionLUKS2*
 else
-  ceph_test_librbd
+  ceph_test_librbd --gtest_filter=*TestLibRBD.TestEncryptionLUKS2*
 fi
 exit 0

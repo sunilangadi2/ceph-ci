707daf1436b76e4e430c1622b1c3c91e29908b9c
Sebastian Wagner <sewagner@redhat.com>
2021-06-02 16:04:21 +0200

    mgr/nfs: flake8
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

--- src/pybind/mgr/nfs/cluster.py
+++ src/pybind/mgr/nfs/cluster.py
@@ -1,5 +1,4 @@
 import logging
-import socket
 import json
 import re
 from typing import cast, Dict, List, Any, Union, Optional, TypeVar, Callable, TYPE_CHECKING, Tuple
@@ -221,7 +220,7 @@ class NFSCluster:
             raise ClusterNotFound()
         except NotImplementedError:
             return 0, "NFS-Ganesha Config Added Successfully "\
-                    "(Manual Restart of NFS PODS required)", ""
+                "(Manual Restart of NFS PODS required)", ""
         except Exception as e:
             return exception_handler(e, f"Setting NFS-Ganesha Config failed for {cluster_id}")
 
@@ -239,6 +238,6 @@ class NFSCluster:
             raise ClusterNotFound()
         except NotImplementedError:
             return 0, "NFS-Ganesha Config Removed Successfully "\
-                    "(Manual Restart of NFS PODS required)", ""
+                "(Manual Restart of NFS PODS required)", ""
         except Exception as e:
             return exception_handler(e, f"Resetting NFS-Ganesha Config failed for {cluster_id}")

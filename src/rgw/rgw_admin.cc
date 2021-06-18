2e5b14640b9cd8109236312ee22b4285b197fdf5
Kalpesh Pandya <kapandya@redhat.com>
2021-06-18 13:51:30 +0530

    src/rgw: DPP addition to rgw_bucket_sync.cc
    Signed-off-by: Kalpesh Pandya <kapandya@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2446,7 +2446,7 @@ static int sync_info(std::optional<rgw_zone_id> opt_target_zone, std::optional<r
   set<rgw_zone_id> source_zones;
   set<rgw_zone_id> target_zones;
 
-  zone_policy_handler->reflect(nullptr, nullptr,
+  zone_policy_handler->reflect(dpp(), nullptr, nullptr,
                                nullptr, nullptr,
                                &source_zones,
                                &target_zones,

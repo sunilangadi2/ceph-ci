11b4b05efa7e4ab577be24ff43451b1d21ebf8c3
Mark Kogan <mkogan@redhat.com>
2021-06-23 15:55:07 +0300

    rgw: d3n: addressing latest review comments
    Signed-off-by: Mark Kogan <mkogan@redhat.com>

--- src/rgw/rgw_aio.h
+++ src/rgw/rgw_aio.h
@@ -99,8 +99,8 @@ class Aio {
                             optional_yield y);
   static OpFunc librados_op(librados::ObjectWriteOperation&& op,
                             optional_yield y);
-  static OpFunc d3n_cache_op(librados::ObjectReadOperation&& op, optional_yield y,
-                             off_t obj_ofs, off_t read_ofs, off_t read_len, std::string& location);
+  static OpFunc d3n_cache_op(optional_yield y,
+                             off_t read_ofs, off_t read_len, std::string& location);
 };
 
 } // namespace rgw

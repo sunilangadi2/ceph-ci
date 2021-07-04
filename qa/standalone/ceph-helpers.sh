adc5b936e274e40e504e9486b91e82872cd58ff1
Sridhar Seshasayee <sseshasa@redhat.com>
2021-07-04 23:22:48 +0530

    qa/standalone: Set default scheduler to wpq
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>

--- qa/standalone/ceph-helpers.sh
+++ qa/standalone/ceph-helpers.sh
@@ -642,7 +642,7 @@ function run_osd() {
     ceph_args+=" --osd-scrub-load-threshold=2000"
     ceph_args+=" --osd-data=$osd_data"
     ceph_args+=" --osd-journal=${osd_data}/journal"
-    #ceph_args+=" --osd-op-queue=wpq"
+    ceph_args+=" --osd-op-queue=wpq"
     ceph_args+=" --chdir="
     ceph_args+=$EXTRA_OPTS
     ceph_args+=" --run-dir=$dir"
@@ -698,7 +698,7 @@ function run_osd_filestore() {
     ceph_args+=" --osd-scrub-load-threshold=2000"
     ceph_args+=" --osd-data=$osd_data"
     ceph_args+=" --osd-journal=${osd_data}/journal"
-    #ceph_args+=" --osd-op-queue=wpq"
+    ceph_args+=" --osd-op-queue=wpq"
     ceph_args+=" --chdir="
     ceph_args+=$EXTRA_OPTS
     ceph_args+=" --run-dir=$dir"
@@ -866,7 +866,7 @@ function activate_osd() {
     ceph_args+=" --osd-scrub-load-threshold=2000"
     ceph_args+=" --osd-data=$osd_data"
     ceph_args+=" --osd-journal=${osd_data}/journal"
-    #ceph_args+=" --osd-op-queue=wpq"
+    ceph_args+=" --osd-op-queue=wpq"
     ceph_args+=" --chdir="
     ceph_args+=$EXTRA_OPTS
     ceph_args+=" --run-dir=$dir"

ceph/ceph

commit 8e15ed7263f84ad955c0172d62420dba75e70d4e

mgr/cephadm: avoid saving daemons of unknown type
Fixes: https://tracker.ceph.com/issues/51176
Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

src/pybind/mgr/cephadm/serve.py
@@ -405,6 +405,10 @@ class CephadmServe:
                 if v:
                     setattr(sd, k, str_to_datetime(d[k]))
             sd.daemon_type = d['name'].split('.')[0]
+            if sd.daemon_type not in ServiceSpec.KNOWN_SERVICE_TYPES:
+                logger.warning(f"Found unknown service type {sd.daemon_type} on host {host}")
+                continue
+
             sd.daemon_id = '.'.join(d['name'].split('.')[1:])
             sd.hostname = host
             sd.container_id = d.get('container_id')
@@ -423,7 +427,8 @@ class CephadmServe:
             sd.ports = d.get('ports')
             sd.ip = d.get('ip')
             sd.rank = int(d['rank']) if d.get('rank') is not None else None
-            sd.rank_generation = int(d['rank_generation']) if d.get('rank_generation') is not None else None
+            sd.rank_generation = int(d['rank_generation']) if d.get(
+                'rank_generation') is not None else None
             if sd.daemon_type == 'osd':
                 sd.osdspec_affinity = self.mgr.osd_service.get_osdspec_affinity(sd.daemon_id)
             if 'state' in d:

src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -234,15 +234,22 @@ class TestCephadm(object):
                 container_id='container_id',
                 version='version',
                 state='running',
-            )
+            ),
+            dict(
+                name='something.foo.bar',
+                style='cephadm',
+                fsid='fsid',
+            ),
+
         ])
     ))
     def test_list_daemons(self, cephadm_module: CephadmOrchestrator):
         cephadm_module.service_cache_timeout = 10
         with with_host(cephadm_module, 'test'):
             CephadmServe(cephadm_module)._refresh_host_daemons('test')
-            c = cephadm_module.list_daemons()
-            assert wait(cephadm_module, c)[0].name() == 'rgw.myrgw.foobar'
+            dds = wait(cephadm_module, cephadm_module.list_daemons())
+            assert len(dds) == 1
+            assert dds[0].name() == 'rgw.myrgw.foobar'
 
     @mock.patch("cephadm.serve.CephadmServe._run_cephadm", _run_cephadm('[]'))
     def test_daemon_action(self, cephadm_module: CephadmOrchestrator):

sunilangadi2/ceph-ci

commit aca9e75a035dd464363dc31d19b10fbc1f06bd8f

mgr/cephadm: fix daemon inventory breakage
8e15ed7263f84ad955c0172d62420dba75e70d4e tried to ignore unknown daemon
types using a list of service types; use a list of daemon types instead.

Notably, include haproxy and keepalived here.

Fixes: 8e15ed7263f84ad955c0172d62420dba75e70d4e
Signed-off-by: Sage Weil <sage@newdream.net>

src/pybind/mgr/cephadm/serve.py
@@ -26,7 +26,7 @@ from cephadm.services.cephadmservice import CephadmDaemonDeploySpec
 from cephadm.schedule import HostAssignment
 from cephadm.autotune import MemoryAutotuner
 from cephadm.utils import forall_hosts, cephadmNoImage, is_repo_digest, \
-    CephadmNoImage, CEPH_TYPES, ContainerInspectInfo
+    CephadmNoImage, CEPH_TYPES, ContainerInspectInfo, KNOWN_DAEMON_TYPES
 from mgr_module import MonCommandFailed
 from mgr_util import format_bytes
 
@@ -405,7 +405,7 @@ class CephadmServe:
                 if v:
                     setattr(sd, k, str_to_datetime(d[k]))
             sd.daemon_type = d['name'].split('.')[0]
-            if sd.daemon_type not in ServiceSpec.KNOWN_SERVICE_TYPES:
+            if sd.daemon_type not in KNOWN_DAEMON_TYPES:
                 logger.warning(f"Found unknown service type {sd.daemon_type} on host {host}")
                 continue
 

src/pybind/mgr/cephadm/utils.py
@@ -22,9 +22,10 @@ class CephadmNoImage(Enum):
 # ceph daemon types that use the ceph container image.
 # NOTE: order important here as these are used for upgrade order
 CEPH_TYPES = ['mgr', 'mon', 'crash', 'osd', 'mds', 'rgw', 'rbd-mirror', 'cephfs-mirror']
-GATEWAY_TYPES = ['iscsi', 'nfs']
+GATEWAY_TYPES = ['iscsi', 'nfs', 'keepalived', 'haproxy']
 MONITORING_STACK_TYPES = ['node-exporter', 'prometheus', 'alertmanager', 'grafana']
 CEPH_UPGRADE_ORDER = CEPH_TYPES + GATEWAY_TYPES + MONITORING_STACK_TYPES
+KNOWN_DAEMON_TYPES = CEPH_UPGRADE_ORDER
 
 
 # Used for _run_cephadm used for check-host etc that don't require an --image parameter
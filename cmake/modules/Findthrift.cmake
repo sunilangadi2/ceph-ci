512ef7980ee3094a5928914786efb73ce479be23
Deepika Upadhyay <dupadhya@redhat.com>
2021-07-20 12:19:33 +0000

    debian,cmake: remove thrift-compiler as a jaeger dependency
    * jaeger CMakeList actually doesn't make use of thrift complier but
    still had it as a unused dependency, this change addresses it's removal.
    
    * bump up submodule version for jaeger-client-cpp for thrift compiler
    removal
    
    * debian remove thrift as build dependency
    
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

--- cmake/modules/Findthrift.cmake
+++ cmake/modules/Findthrift.cmake
@@ -1,6 +1,6 @@
 # This module defines thrift_LIBRARIES, libraries to link thrift_INCLUDE_DIR,
 # where to find thrift headers thrift_COMPILER, thrift compiler executable
-# thrift_FOUND, If false, do not try to use ant
+# thrift_FOUND, If false, do not try to use it.
 
 # prefer the thrift version supplied in thrift_HOME (cmake -Dthrift_HOME then
 # environment)
@@ -17,12 +17,6 @@ find_library(
   HINTS ${thrift_HOME} ENV thrift_HOME /usr/local /opt/local
   PATH_SUFFIXES lib lib64)
 
-find_program(
-  thrift_COMPILER
-  NAMES thrift
-  HINTS ${thrift_HOME} ENV thrift_HOME /usr/local /opt/local
-  PATH_SUFFIXES bin bin64)
-
 if(thrift_FOUND AND NOT (TARGET thrift::libthrift))
   add_library(thrift::libthrift UNKNOWN IMPORTED)
 
@@ -34,5 +28,5 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(thrift DEFAULT_MSG thrift_LIBRARIES
-                                  thrift_INCLUDE_DIR thrift_COMPILER)
-mark_as_advanced(thrift_LIBRARIES thrift_INCLUDE_DIR thrift_COMPILER)
+                                  thrift_INCLUDE_DIR)
+mark_as_advanced(thrift_LIBRARIES thrift_INCLUDE_DIR)

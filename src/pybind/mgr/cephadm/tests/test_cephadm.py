d182d163be3b1714c9da5da60981509efa73e3bd
jianglong01 <jianglong01@qianxin.com>
2021-08-04 18:39:44 +0800

    mgr/cephadm: fix pytest.fixture error when exec with_cephadm_module
    Replace get_osdspec_affinity with get_osd_metadata because of the former is removed.
    
    Signed-off-by: jianglong01 <jianglong01@qianxin.com>

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -246,7 +246,7 @@ class TestCephadm(object):
     def test_list_daemons(self, cephadm_module: CephadmOrchestrator):
         cephadm_module.service_cache_timeout = 10
         with with_host(cephadm_module, 'test'):
-            CephadmServe(cephadm_module)._refresh_host_daemons('test')
+            CephadmServe(cephadm_module)._refresh_host_daemons('test', {})
             dds = wait(cephadm_module, cephadm_module.list_daemons())
             assert len(dds) == 1
             assert dds[0].name() == 'rgw.myrgw.foobar'
@@ -748,7 +748,7 @@ spec:
     @mock.patch("cephadm.services.osd.RemoveUtil.get_pg_count", lambda _, __: 0)
     def test_remove_osds(self, cephadm_module):
         with with_host(cephadm_module, 'test'):
-            CephadmServe(cephadm_module)._refresh_host_daemons('test')
+            CephadmServe(cephadm_module)._refresh_host_daemons('test', {})
             c = cephadm_module.list_daemons()
             wait(cephadm_module, c)
 
@@ -803,7 +803,7 @@ spec:
     ))
     def test_remove_daemon(self, cephadm_module):
         with with_host(cephadm_module, 'test'):
-            CephadmServe(cephadm_module)._refresh_host_daemons('test')
+            CephadmServe(cephadm_module)._refresh_host_daemons('test', {})
             c = cephadm_module.list_daemons()
             wait(cephadm_module, c)
             c = cephadm_module.remove_daemons(['rgw.myrgw.myhost.myid'])

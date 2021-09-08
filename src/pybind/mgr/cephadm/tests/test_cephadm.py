ef23ba40195697461edb1f22fd8e4339792a5ed0
Sebastian Wagner <sewagner@redhat.com>
2021-09-08 16:23:32 +0200

    mgr/cephadm: check_for_moved_osds: Add OrchEvent
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -21,7 +21,7 @@ from ceph.deployment.drive_selection.selector import DriveSelection
 from ceph.deployment.inventory import Devices, Device
 from ceph.utils import datetime_to_str, datetime_now
 from orchestrator import DaemonDescription, InventoryHost, \
-    HostSpec, OrchestratorError, DaemonDescriptionStatus
+    HostSpec, OrchestratorError, DaemonDescriptionStatus, OrchestratorEvent
 from tests import mock
 from .fixtures import wait, _run_cephadm, match_glob, with_host, \
     with_cephadm_module, with_service, _deploy_cephadm_binary
@@ -872,6 +872,15 @@ spec:
                                 'prefix': 'auth rm',
                                 'entity': 'osd.1',
                             })
+
+                        assert cephadm_module.events.get_for_daemon('osd.1') == [
+                            OrchestratorEvent(mock.ANY, 'daemon', 'osd.1', 'INFO',
+                                              "Deployed osd.1 on host 'host1'"),
+                            OrchestratorEvent(mock.ANY, 'daemon', 'osd.1', 'INFO',
+                                              "Deployed osd.1 on host 'host2'"),
+                            OrchestratorEvent(mock.ANY, 'daemon', 'osd.1', 'INFO',
+                                              "Removed duplicated daemon on host 'host2'"),
+                        ]
 
     @pytest.mark.parametrize(
         "spec",

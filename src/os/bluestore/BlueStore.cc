43dc4a652d788f6ddcb7c8040d38b91c59cb0bff
Sage Weil <sage@newdream.net>
2021-10-19 15:09:37 -0500

    os/bluestore: fix cleaner race with collection removal
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -13161,6 +13161,10 @@ void BlueStore::_clean_some(ghobject_t oid, uint32_t zone)
 	   << dendl;
 
   CollectionRef cref = _get_collection_by_oid(oid);
+  if (!cref) {
+    dout(10) << __func__ << " can't find collection for " << oid << dendl;
+    return;
+  }
   Collection *c = cref.get();
 
   // serialize io dispatch vs other transactions
@@ -13169,7 +13173,7 @@ void BlueStore::_clean_some(ghobject_t oid, uint32_t zone)
 
   auto o = c->get_onode(oid, false);
   if (!o) {
-    derr << __func__ << " can't find " << oid << dendl;
+    dout(10) << __func__ << " can't find " << oid << dendl;
     return;
   }
 
